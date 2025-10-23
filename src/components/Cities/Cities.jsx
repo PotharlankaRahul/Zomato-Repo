9f652cd9b31c07a702fc4899f969b14308bc8c8b
PotharlankaRahul <121004749+PotharlankaRahul@users.noreply.github.com>
2025-10-23 13:04:05 +0530

    Update Cities.jsx

--- src/components/Cities/Cities.jsx
+++ src/components/Cities/Cities.jsx
@@ -7,7 +7,7 @@ const Cities = () => {
   return (
     <div className="cities">
       <h1>
-        Popular localities in and around <span>Bangalore</span>
+        Popular localities in and around <span> Hyderabad </span>
       </h1>
       <div className="cityContainer">
         <div className="city">
